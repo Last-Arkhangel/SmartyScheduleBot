46112ef63d0b7cb405bb824fedcee2ae5a95b129
Vitaliy <koocherov@ukr.net>
2018-08-01 11:39:15 +0300

    Updated newsletter distribution system

--- utils/newsletter_for_users.py
+++ utils/newsletter_for_users.py
@@ -3,22 +3,38 @@ import time
 import sqlite3
 
 
-TOKEN = ''
-connection = sqlite3.connect('users_db.sqlite', check_same_thread=False)
+def write_log(message):
+
+    with open('file_log.txt', 'a') as f:
+        f.write(message + '\n')
+
+TOKEN = '<TOKEN>'
+connection = sqlite3.connect('SmartyS_DB.sqlite', check_same_thread=False)
 
 cursor = connection.cursor()
-cursor.execute("""SELECT t_id FROM users""")
+cursor.execute("""SELECT * FROM users""")
 connection.commit()
-rez = cursor.fetchall()
+users_list = cursor.fetchall()
 cursor.close()
 
+ignore_users = ['', '']
 
-msg = 'Some message'
+msg = 'Hello'
 
 i = 1
 
-for _id in rez:
-    r = requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(TOKEN, _id[0], msg))
-    print(i, _id[0], r.status_code)
+for u_id in users_list:
+
+    if u_id[0] not in ignore_users:
+        r = requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.
+                         format(TOKEN, u_id[0], msg))
+
+        m = ("({} / {}) - {} {} {} [{}]".format(i, len(users_list), u_id[0], u_id[2], u_id[3] or '', r.status_code))
+        time.sleep(0.15)
+
+    else:
+        m = "({} / {}) - {} {} {} [Ignore]".format(i, len(users_list), u_id[0], u_id[2], u_id[3] or '')
+
+    print(m)
+    write_log(m)
     i = i + 1
-    time.sleep(0.3)
